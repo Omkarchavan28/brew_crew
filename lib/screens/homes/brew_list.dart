df28db448ad35290cb9461c49dcbf6ad33097e8d
Omkarchavan28 <chavanomkar245@gmail.com>
2019-12-06 19:24:39 +0530

    fixed null error

--- lib/screens/homes/brew_list.dart
+++ lib/screens/homes/brew_list.dart
@@ -12,19 +12,17 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<List<Brew>>(context);
+    final brews = Provider.of<List<Brew>>(context) ?? [];
 
     //print(brews.documents);
 
-    return brews != null
-        ? Container(
-            child: ListView.builder(
-              itemCount: brews.length,
-              itemBuilder: (context, index) {
-                return BrewTile(brew: brews[index]);
-              },
-            ),
-          )
-        : Container();
+    return Container(
+      child: ListView.builder(
+        itemCount: brews.length,
+        itemBuilder: (context, index) {
+          return BrewTile(brew: brews[index]);
+        },
+      ),
+    );
   }
 }
