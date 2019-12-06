f4d613fd76b3d8e02885c97270c7055cb23fac88
Omkarchavan28 <chavanomkar245@gmail.com>
2019-12-06 16:39:22 +0530

    Brew Stream added

--- lib/screens/homes/home.dart
+++ lib/screens/homes/home.dart
@@ -1,27 +1,35 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:provider/provider.dart';
+
+import 'brew_list.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[200],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        title: Text('Brew Crew'),
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: () async {
-              await _auth.signOut();
-            },
-            icon: Icon(Icons.people),
-            label: Text('Log Out'),
-          )
-        ],
+    return StreamProvider<List<Brew>>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[200],
+        appBar: AppBar(
+          backgroundColor: Colors.brown[400],
+          title: Text('Brew Crew'),
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              onPressed: () async {
+                await _auth.signOut();
+              },
+              icon: Icon(Icons.people),
+              label: Text('Log Out'),
+            )
+          ],
+        ),
+        body: BrewList(),
       ),
-      body: Text('Home'),
     );
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
@@ -6,7 +7,25 @@ class DatabaseService {
   //collection reference
   final CollectionReference brewCollection =
       Firestore.instance.collection('brews');
+
   Future updateUserData(String sugars, String name, int strength) async {
-    return await brewCollection.document(uid);
+    return await brewCollection
+        .document(uid)
+        .setData({'sugars': sugars, 'name': name, 'strength': strength});
+  }
+
+//brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Brew(
+          name: doc.data['name'] ?? '',
+          sugars: doc.data['sugars'] ?? '',
+          strength: doc.data['strength'] ?? 0);
+    }).toList();
+  }
+
+  //get brew stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
   }
 }
