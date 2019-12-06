f6d9955d1c822fb54a2470e46a4c4395057247dc
Omkarchavan28 <chavanomkar245@gmail.com>
2019-12-06 12:33:38 +0530

    Comment added

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:provider/provider.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of application.
   @override
   Widget build(BuildContext context) {
     return StreamProvider.value(
